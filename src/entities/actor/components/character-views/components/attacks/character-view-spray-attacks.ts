05d2a9fcbf97d4abd84655c9b8b49a9d0ff80033
Bubz43 <sicert43@gmail.com>
2021-03-14 17:24:16 -0400

    Add spray attacks element (WIP)

---
+++ src/entities/actor/components/character-views/components/attacks/character-view-spray-attacks.ts
@@ -0,0 +1,22 @@
+import type { SprayWeapon } from '@src/entities/item/proxies/spray-weapon';
+import { customElement, LitElement, property } from 'lit-element';
+import styles from './character-view-spray-attacks.scss';
+
+@customElement('character-view-spray-attacks')
+export class CharacterViewSprayAttacks extends LitElement {
+  static get is() {
+    return 'character-view-spray-attacks' as const;
+  }
+
+  static get styles() {
+    return [styles];
+  }
+
+  @property({ attribute: false }) weapon!: SprayWeapon;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'character-view-spray-attacks': CharacterViewSprayAttacks;
+  }
+}
