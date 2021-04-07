010e129d1301a43e05479c05324eea25ad388ce0
Bubz43 <sicert43@gmail.com>
2021-04-07 14:19:30 -0400

    Don't overlapy health-item parts

--- src/health/components/health-item/health-item.ts
+++ src/health/components/health-item/health-item.ts
@@ -30,8 +30,6 @@ export class HealthItem<T extends Health = Health> extends mix(LitElement).with(
 
   @property({ type: Boolean, reflect: true }) disabled = false;
 
-  @property({ type: Boolean }) mini = false;
-
   @query('.view', true) private viewElement?: HTMLElement;
 
   focus() {
@@ -49,7 +47,7 @@ export class HealthItem<T extends Health = Health> extends mix(LitElement).with(
   }
 
   render() {
-    const { health, clickable, mini } = this;
+    const { health, clickable } = this;
     const { type, main, wound, icon, damagePercents } = health;
     const { durability, deathRating, dead } = damagePercents;
     return html`
@@ -64,84 +62,43 @@ export class HealthItem<T extends Health = Health> extends mix(LitElement).with(
         @focus="${this.handleRippleFocus}"
         @blur="${this.handleRippleBlur}"
       >
+        <img
+          src=${icon}
+          class="health-icon"
+          title="${localize(type)} ${localize('health')}"
+        />
+
+        ${health.regenState
+          ? html`
+              <img
+                class="regen-available ${classMap({
+                  ready: health.readyRegen,
+                })}"
+                height="16px"
+                src=${localImage('icons/health/auto-repair.svg')}
+              />
+            `
+          : ''}
+
         <div class="info">
           <div class="health-type" title=${health.source}>
             <slot name="source">${health.source}</slot>
           </div>
-
-          <section class="damage-info">
-            ${health.regenState
-              ? html`
-                  <img
-                    class="regen-available ${classMap({
-                      ready: health.readyRegen,
-                    })}"
-                    height="22px"
-                    src=${localImage('icons/health/auto-repair.svg')}
-                  />
-                `
-              : ''}
-            ${mini
-              ? ''
-              : html`<div
-                  class="damage ${classMap({
-                    dying: durability >= 1,
-                    dead,
-                  })}"
-                >
-                  ${compact([
-                    main.damage,
-                    main.durability,
-                    main.deathRating,
-                  ]).map(
-                    ({ label, value }) =>
-                      html` <span title=${label}>${value}</span> `,
-                  )}
-                </div>`}
-            ${wound
-              ? html`
-                  <section class="wounds">
-                    <div title=${wound.wounds.label}>
-                      <img class="wound-icon" src=${health.woundIcon} /><span
-                        class="wound-value"
-                        >${wound.wounds.value}</span
-                      >
-                    </div>
-                    ${wound.woundsIgnored.value
-                      ? html`
-                          <div title=${wound.woundsIgnored.label}>
-                            <img
-                              src=${localImage('icons/health/interdiction.svg')}
-                              class="wound-icon ignored"
-                            /><span class="wound-value"
-                              >${wound.woundsIgnored.value}</span
-                            >
-                          </div>
-                        `
-                      : ''}
-                    ${mini
-                      ? ''
-                      : html`
-                          <div
-                            class="wound-threshold"
-                            title=${wound.woundThreshold.label}
-                          >
-                            <span class="threshold-label"
-                              >${localize(
-                                'SHORT',
-                                wound.woundThreshold.prop,
-                              )}</span
-                            >
-                            <span class="wound-value"
-                              >${wound.woundThreshold.value}</span
-                            >
-                          </div>
-                        `}
-                  </section>
-                `
-              : ''}
-          </section>
         </div>
+
+        <section class="damage-info">
+          <div
+            class="damage ${classMap({
+              dying: durability >= 1,
+              dead,
+            })}"
+          >
+            ${compact([main.damage, main.durability, main.deathRating]).map(
+              ({ label, value }) =>
+                html` <span title=${label}>${value}</span> `,
+            )}
+          </div>
+        </section>
 
         <div class="bars ${classMap({ dead })}">
           <div
@@ -156,12 +113,30 @@ export class HealthItem<T extends Health = Health> extends mix(LitElement).with(
             : ''}
         </div>
 
-        <img
-          src=${icon}
-          class="health-icon"
-          title="${localize(type)} ${localize('health')}"
-        />
-
+        ${wound
+          ? html`
+              <section class="wounds">
+                <div title=${wound.wounds.label}>
+                  <img class="wound-icon" src=${health.woundIcon} /><span
+                    class="wound-value"
+                    >${wound.wounds.value}</span
+                  >
+                </div>
+                ${wound.woundsIgnored.value
+                  ? html`
+                      <div title=${wound.woundsIgnored.label}>
+                        <img
+                          src=${localImage('icons/health/interdiction.svg')}
+                          class="wound-icon ignored"
+                        /><span class="wound-value"
+                          >${wound.woundsIgnored.value}</span
+                        >
+                      </div>
+                    `
+                  : ''}
+              </section>
+            `
+          : ''}
         ${this.renderRipple(this.disabled || !this.clickable)}
       </div>
     `;
